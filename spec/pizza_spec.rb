5b093526e02036710bdd39515a6f79115dcb67cf
John Reagan Moore <johnreaganmoore@gmail.com>
2013-09-11 10:15:52 -0500

    Adds a failing test for Pizza::Topping.initialize.

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -8,5 +8,11 @@ describe Pizza::Pie do
 end 
 
 describe Pizza::Topping do
+  describe '.initialize' do
+    it 'sets the name of a new topping' do
+      topping = Pizza::Topping.new('olives')
 
+      expect(topping.name).to eq('olives')
+    end
+  end
 end
